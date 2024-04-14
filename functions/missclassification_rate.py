6c624d77129d131d26a9c4005b2132a5874d1f0a
JakubKozlowski <20681187+hitsu12@users.noreply.github.com>
2024-04-14 21:00:03 +0200

    Logistic regression done

---
+++ functions/missclassification_rate.py
@@ -0,0 +1,7 @@
+def missclassification_rate(y_test, y_pred):
+    from sklearn.metrics import confusion_matrix
+    cm = confusion_matrix(y_test, y_pred)
+    fn = cm[1][0]
+    fp = cm[0][1]
+    rate = (fn+fp) / sum(sum(cm))
+    return rate
